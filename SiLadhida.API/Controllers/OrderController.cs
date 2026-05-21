0bcce5e2f0c8db042b20e663e5067431d6cdcfab
David NTB <davidntb7000@gmail.com>
2026-05-21 16:50:02 +0700

    refactor(api): implement async programming for order endpoints

--- SiLadhida.API/Controllers/OrderController.cs
+++ SiLadhida.API/Controllers/OrderController.cs
@@ -1,10 +1,9 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using SiLadhida.API.Data;
 using SiLadhida.API.DTOs;
 using SiLadhida.Core.Entities;
-using SiLadhida.Core.Enums;
 using SiLadhida.Core.Services;
 
 namespace SiLadhida.API.Controllers
@@ -27,7 +26,7 @@ namespace SiLadhida.API.Controllers
 
         // CREATE ORDER
         [HttpPost]
-        public IActionResult Create([FromBody] CreateOrderDto dto)
+        public async Task<IActionResult> Create([FromBody] CreateOrderDto dto)
         {
             using var transaction = _context.Database.BeginTransaction();
 
@@ -43,7 +42,7 @@ namespace SiLadhida.API.Controllers
 
                 foreach (var itemDto in dto.Items)
                 {
-                    var produk = _context.Produk.Find(itemDto.ProdukId);
+                    var produk = await _context.Produk.FindAsync(itemDto.ProdukId);
 
                     if (produk == null)
                         return BadRequest($"Produk ID {itemDto.ProdukId} tidak ditemukan");
@@ -86,7 +85,7 @@ namespace SiLadhida.API.Controllers
 
                 // throw new Exception("Simulasi gagal");
 
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
 
                 transaction.Commit();
 
@@ -112,20 +111,20 @@ namespace SiLadhida.API.Controllers
 
         // GET ALL
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var data = _context.Pesanan
+            var data = await _context.Pesanan
                 .Include(p => p.Items)
                 .ThenInclude(i => i.Produk) 
-                .ToList();
+                .ToListAsync();
             return Ok(data);
         }
 
         // UPDATE STATUS (STATE-BASED)
         [HttpPut("{id}/status")]
-        public IActionResult UpdateStatus(int id, [FromBody] UpdateStatusDto dto)
+        public async Task<IActionResult> UpdateStatus(int id, [FromBody] UpdateStatusDto dto)
         {
-            var order = _context.Pesanan.Find(id);
+            var order = await _context.Pesanan.FindAsync(id);
 
             if (order == null)
                 return NotFound("Pesanan tidak ditemukan");
@@ -134,10 +133,10 @@ namespace SiLadhida.API.Controllers
                 return BadRequest("Transisi status tidak valid");
 
             order.StatusSekarang = _service.GetNextState(order.StatusSekarang, dto.Trigger);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             _logger.LogInformation(
-                "Status pesanan {OrderId} berubah menjadi {Status}",
+                "Status pesanan {OrderId} | trigger {Trigger} | menjadi {Status}",
                 order.Id,
                 dto.Trigger,
                 order.StatusSekarang
